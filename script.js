0446aeb33c61125e25ab179374f6edbebbc0853d
zidankamil19 <nopalnann@gmail.com>
2026-09-12 11:12:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // GANTI BAGIAN INI
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbzSJW_cBgn6LbjFd6XWlrF16UcoD6eJIuTtReg8K4h4JnmhniKmoyZ6_7Cs8QdW6L9PzA/exec";
+  "https://script.google.com/macros/s/AKfycbwrKSjE_m6dUeQDX0wP2C_gmeG0DGmF-OsbbyfKrnMGm7kkLa1kaR-cMNJ7vLQ_vxqJ5Q/exec";
 
 
 // ========================================
